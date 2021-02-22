aa510308cfd1f14de9c217c254002e4c17698a9d
Vanshika Assudani <vanshika.assudani@coditas.com>
2021-02-22 21:18:55 +0530

    Implemented the add post and error message using conditional rendering

--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import '../components/style.css';
-import {Render } from './Render';
-import{RenderPost} from './Render'
+import { UserList } from './List';
+import { PostList } from './List'
 
 export default class FetchApi extends Component {
     //created a users state.
     state = {
         users: [],
         posts: [],
-        title:'',
-        body:''
+        title: '',
+        body: '',
+        isMessageSuccess: false,
+        isMessageFailure: false
     }
-
 
 
     //in this function the users from being fetched from the api
@@ -28,86 +29,120 @@ export default class FetchApi extends Component {
             .then((postData) => this.setPost(postData)); // Calling setUsers and passing the data.
     }
 
-    addPost=(e)=> {
-      fetch('https://jsonplaceholder.typicode.com/posts', {
+    addPost = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json , text/plain , */*',
                 'Content-type': 'application/json'
             },
-            body: JSON.stringify(this.state)
-    
-        }).then((res) => res.json())
-        .then((data) => this.setAddPost(data))
-        e.preventDefault()
+            body: JSON.stringify({ title: this.state.title, body: this.state.body })
+
+        }).then((res) => res.json)
+            .then((data) => {
+                console.log(data);
+                this.clearInputs()
+
+            }
+
+            )
+
     }
-    
-    setAddPost=(data)=>{
-        const name=data.target.name
-        const value=data.target.value
-        this.setState({
-            [name]:value
-             },
-             )
+
+
+    setAddPost = (data) => {
+        const name = data.target.name
+        const value = data.target.value
+        this.setState({ [name]: value })
     }
-  
+
     setUsers = (data) => {
         //created a new function to set the state of users
-        this.setState({
-            users: data
-        })
+        this.setState({ users: data })
     }
 
-    setPost = (postData) => {
-        this.setState({
-            posts: postData
-        })
-    }
+    setPost = (postData) => this.setState({ posts: postData })
 
-   
-
-submitHandler=(e)=>{
-    e.preventDefault()
-    console.log(this.state.title,this.state.body)
-}
-
-  //Create a functional component for rendering users 
-    renderUser = () => {
-        return this.state.users.map((user) => {
-         return    <Render user={user} />  })
-        //we have removed join from here because in react everything is javascript and we are using JSX.
-    }
 
     renderPost = () => {
-        return this.state.posts.map((post) => {
-    return <RenderPost post={post}/>
-        })
+        return this.state.posts.map((post, index) => <PostList key={index} post={post} />)
         //we have removed join from here because in react everything is javascript and we are using JSX.
     }
 
+    clearInputs = () => {
+        this.setState({
+            title: '',
+            body: '',
+            isMessageSuccess: !this.state.isMessageSuccess
+
+        })
+
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        const {title,body} =this.state
+        if ((title === '' && body === '') || (title === '') || (body === '')) {
+            this.setState({ isMessageFailure: !this.state.isMessageFailure })
+            return          
+        }
+        if (title !== '' && body !== '') {
+            if (this.state.isMessageFailure) {
+                //toggled the boolean variable by using the not operator for it's state
+                this.setState({
+                    isMessageSuccess: !this.state.isMessageSuccess,
+                    isMessageFailure: !this.state.isMessageFailure
+                })
+                this.addPost()
+                return 
+              }
+              if(!this.state.isMessageFailure){
+                  this.setState({
+                      isMessageSuccess:!this.state.isMessageSuccess
+                  })
+                  this.addPost()
+                  return 
+              }
+                 
+        }
+
+    }
+
+
+
+
+    //Create a functional component for rendering users 
+    renderUser = () => {
+        return this.state.users.map((user, index) => <UserList key={index} user={user} />)
+        //we have removed join from here because in react everything is javascript and we are using JSX.
+    }
+
+
+
     render() {
-    
+
         return (
             <div>
                 <nav className="navBar">
-                    <button onClick= {()=>this.fetchUserHandler()} className="btn" id="getUser">Fetch User</button>
+                    <button onClick={() => this.fetchUserHandler()} className="btn" id="getUser">Fetch User</button>
                     <button onClick={this.fetchPostHandler} className="btn" id="getPost">Fetch Post</button>
                     <div>
-                        <button onClick={this.submitHandler} className="btn" id="addPost1">Add Post</button></div>
+                        <button className="btn" id="addPost1">Add Post</button></div>
                 </nav>
                 <div id="output"> {this.renderPost()} {this.renderUser()} </div>
 
-                <form onSubmit={this.addPost} className="addPostForm ">
+                <form className="addPostForm ">
                     <div className="postDiv">
                         <h3>Add Posts</h3>
-                        <span className="notification messageInvisible">Post Added</span>
+                        {this.state.isMessageSuccess && <span className="notification">Post Added</span>}
+                        {this.state.isMessageFailure && <span className="notification__warning">Please enter all fields</span>}
 
                     </div>
                     <div className="inputWrapper">
-                        <input onChange={this.setAddPost}  value={this.state.title} name="title" type="text" id="title" placeholder="Title" />
+                        <input onChange={this.setAddPost} value={this.state.title} name="title" type="text" id="title" placeholder="Title" />
                         <input onChange={this.setAddPost} value={this.state.body} type="text" name="body" placeholder="Body" id="body" />
                     </div>
-                    <button onClick={this.submitHandler}  type="submit" id="submit">Submit</button>
+                    <button onClick={this.submitHandler} type="submit" id="submit">Submit</button>
                 </form>
             </div>
 
